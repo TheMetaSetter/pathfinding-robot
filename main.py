ad2333dc575a51645965dc7f498690f4b26fe89e
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2024-03-08 18:37:54 +0700

    Modify 'main.py'
    Modify #1

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from map_and_obstacles import Map2d
 from map_file_reader import MapFileReader
 from solver import DijkstraSolver
-from solution import Solution2d
 from visualizer import Visualizer2d
 
 map_file_reader = MapFileReader("input/input.txt")
