54716a0702913d2041e26f89fe8f3f3405040e02
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2024-03-09 23:38:06 +0700

    Modify A_asteriskSolver and runtime measurement
    Modify#2

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from map_file_reader import MapFileReader
 from solver import DijkstraSolver
-from solver import A_asterickSolver
+from solver import A_asteriskSolver
 from visualizer import Visualizer2d
-from time import time
 
 map_file_reader = MapFileReader("input/input.txt")
 map = map_file_reader.readMap2d()
 
-start = time()
-solver = A_asterickSolver()
+solver = DijkstraSolver()
 solution = map.solvedBy(solver)
 solution.showToConsole()
-end = time()
-print("Runtime =", end - start)
 
 visualizer = Visualizer2d(solution, map)
 visualizer.visualize2d()
