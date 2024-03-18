ae95f5577d201b61dec86207e283f8d9b09efa2e
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2024-03-19 01:30:10 +0700

    Modify evaluate_genetic_algorithm.py

--- evaluate_genetic_algorithm.py
+++ evaluate_genetic_algorithm.py
@@ -1,41 +1,20 @@
+import threading
+
 if __name__ == "__main__":
     from map_file_reader import MapFileReader
     from solver import GASolver
-    
-    # Test performance of GASolver
     import numpy as np
+    import matplotlib.pyplot as plt
 
     map_file_reader = MapFileReader("input/tsp_static_obstacles_2.txt")
     map = map_file_reader.readMap2d()
     
-    average_volatilities = [] # Store the average fitness score of each generation in each run
-    best_volatilities = [] # Store the best fitness score of each generation in each run
+    solver = GASolver(num_generations=500, num_of_parents=200, sol_per_pop=700, mutation_probability=(0.8,0.2))
     
-    for i in range(100):
-        solver = GASolver(num_generations=300, num_of_parents=200, sol_per_pop=700, mutation_probability=(0.8,0.2))
-        solution, generations_averages, generations_bests = map.solvedBy(solver)
-        
-        # Measure the volatility of the generations_average
-        average_volatility = np.std(generations_averages)
-        
-        # Measure the volatility of the generations_best
-        best_volatility = np.std(generations_bests)
-        
-        average_volatilities.append(average_volatility)
-        best_volatilities.append(best_volatility)
-        
-        # solution.showToConsole()
-        
-    # Plot the average and best volatilities of each run on different plots
-    import matplotlib.pyplot as plt
-    plt.plot(average_volatilities)
-    plt.xlabel("Run")
-    plt.ylabel("Average Volatility")
-    plt.savefig("average_volatility.png")
+    solution, generations_averages, generations_bests = map.solvedBy(solver)
     
-    plt.clf()
-    
-    plt.plot(best_volatilities)
-    plt.xlabel("Run")
-    plt.ylabel("Best Volatility")
-    plt.savefig("best_volatility.png")
+    plt.plot(generations_bests)
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness Value')
+    plt.title('Best Fitness Value of Each Generation')
+    plt.show()
